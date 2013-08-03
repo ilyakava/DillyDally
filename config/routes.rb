3bdf615b3bb8efbde57af93007f0aefed3aebfdc
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-03 11:39:29 -0400

    Add Collections controller to test devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DillyDally::Application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+  resources :collections, only: [:index]
+  root to: 'collections#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
