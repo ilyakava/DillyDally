457421a6ea5ee81a17e927e8a1cc6acc0f16b020
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-03 10:44:47 -0400

    Make new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DillyDally::Application.config.secret_key_base = 'b099c06d53667ee752c6c9541d0f4e81ece4810646f802259173c92ad87ea41a7404a247772bc9b83e53dac86d69bc84ffd363f664ab4e803446465d571074d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DillyDally::Application.config.session_store :cookie_store, key: '_DillyDally_session'
