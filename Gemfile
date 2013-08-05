1601509898e3700c593b93e7607b5cecfb770c38
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-05 09:43:12 -0400

    Install Backbone

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'devise'
+gem 'backbone-on-rails'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
---
+++ app/assets/javascripts/dilly_dally.js
@@ -0,0 +1,13 @@
+window.DillyDally = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  DillyDally.initialize();
+});
