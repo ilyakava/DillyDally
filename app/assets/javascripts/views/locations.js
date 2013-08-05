20ee93e6194ba79ad8fc4bf1c289400514b0c195
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-05 17:41:31 -0400

    Add click to recenter functionality on search results

--- app/assets/javascripts/views/locations.js
+++ app/assets/javascripts/views/locations.js
@@ -1,23 +1,33 @@
 DD.Views.Locations = Backbone.View.extend({
   events: {
-    "click button#recenter": "recenterMap"
+    "click button#recenter": "recenterMapForm",
+    "click p.location": "recenterMapResult"
   },
 
   render: function () {
     var that = this;
     var renderedContent = JST['locations/index']();
     
-
     that.$el.html(renderedContent);
     return that;
   },
 
   moveMap: function (locModel) {
-    var latLng = new L.LatLng(locModel.lat, locModel.lng);
+    var latLng = locModel.get("latLng");
+    latLng = new L.LatLng(latLng.lat, latLng.lng);
     map.setView(latLng, 15);
   },
 
-  recenterMap: function () {
+  recenterMapResult: function () {
+    var that = this;
+    console.log("Kaaa!");
+    // console.log($(event.target));
+    var clickId = parseInt($(event.target).attr('id'), 10);
+    window.SR = that.searchResults;
+    that.moveMap(that.searchResults.findWhere({'myId':clickId}));
+  },
+
+  recenterMapForm: function () {
     var that = this;
     console.log("in BB view!");
 
@@ -25,9 +35,11 @@ DD.Views.Locations = Backbone.View.extend({
       var recenterLoc = new DD.Collections.Locations();
 
       // parses array of mapquest objects into BB
+      var index = 0;
       _(mqObjArray).each(function (locObj) {
+        index++;
         recenterLoc.add({
-          test: "hoo",
+          myId: index,
           country: locObj.adminArea1,
           state: locObj.adminArea3,
           city: locObj.adminArea5,
@@ -37,19 +49,22 @@ DD.Views.Locations = Backbone.View.extend({
         });
       });
 
-      // display locations
-      var locList = JST['locations/address_list']({
+      // display and save locations
+      var locListView = JST['locations/address_list']({
         locations: recenterLoc
       });
-      that.$el.find('.data-list').replaceWith(locList);
+      that.$el.find('.data-list').replaceWith(locListView);
+      that.searchResults = recenterLoc;
 
       // sets map center to coord of 1st result
       // coordinates and zoom rating (higher # means more zoomed in)
-      var firstResult = recenterLoc.first().get("latLng");
+      var firstResult = recenterLoc.first();
       that.moveMap(firstResult);
     };
 
+    // grab clicked DOM element
     var address = $(event.target).prev().val().toString();
+    // my method
     var geocoder = new MapQuest.Geocoder( API.MapQuest.key() );
 
     // above callback arg gets array of mapquest objs
