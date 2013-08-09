b23f0346f9a030238c51b4b29bf99cf78286c484
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-08 23:19:33 -0400

    Ensure comment response contains author email

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
 		@comment.update_attribute(:user_id, current_user.id)
 
 		respond_to do |format|
-			format.json { render json: @comment }
+			format.json { render json: @comment.to_json(methods: :author_name) }
 		end
 	end
 
