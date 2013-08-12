6e19e642d1f50c7b271e46b7c87b0cbebdde8f4d
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-12 11:05:48 -0400

    Fix bug in checking private true/false setting

---
+++ app/assets/javascripts/views/comment_form.js
@@ -0,0 +1,40 @@
+DD.Views.CommentForm = Backbone.View.extend({
+  tagName: 'li',
+
+  events: {
+    "click button.submit-comment": "newComment",
+    "click button.remove-comment-form": "hideForm"
+  },
+
+  newComment: function () {
+    var that = this;
+    window.submit = that;
+    console.log("is_private: " + !!that.$("input[name=comment\\[is_private\\]]").is(":checked"));
+
+    var comment = new DD.Models.Comment({
+      body: that.$("textarea[name=comment\\[body\\]]").val(),
+      is_private: (!!that.$("input[name=comment\\[is_private\\]]").is(":checked")),
+      location_id: that.model.get('id')
+    });
+
+    comment.save({}, {
+      success: function (model, response) {
+        // console.log(response);
+        that.model.get('comments').add(response);
+      }
+    });
+    that.hideForm();
+  },
+
+  render: function () {
+    var form = JST['comments/form']();
+    this.$el.html(form);
+    return this;
+  },
+
+  hideForm: function () {
+    $(this.el).undelegate('button.submit-comment', 'click');
+    $(this.el).undelegate('button.remove-comment-form', 'click');
+    this.$el.html("");
+  }
+});
