a43c9736c3a7740fc172b019ebb71f49e43cb1c0
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-12 22:50:55 -0400

    Remember to save, not just .new a Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,8 +2,10 @@ class TagsController < ApplicationController
 
 	def create
 		@tag = Tag.new(params[:tag])
-		respond_to do |format|
-			format.json { render json: @tag }
+		if @tag.save
+			respond_to do |format|
+				format.json { render json: @tag }
+			end
 		end
 	end
 end
