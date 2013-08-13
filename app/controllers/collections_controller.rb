b2c6bebcee81a5f689429227281cfcfcc77be359
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-13 09:48:38 -0400

    Set up bootstrapping collections rails side

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,3 +1,17 @@
 class CollectionsController < ApplicationController
 
+	before_filter :authenticate_user!
+
+	def index
+		@collections = current_user.collections.includes(:locations)
+		@collections_json = @collections.to_json(
+			include: [:locations]
+		)
+
+		respond_to do |format|
+			format.json { render json: @collections_json}
+			format.html { render :index }
+		end
+	end
+
 end
