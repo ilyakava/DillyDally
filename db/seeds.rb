1da7b204b468c8c68c25344a5e1774285e9936a1
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-13 10:06:06 -0400

    Add collection seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(
-	email: "ilya@aol.com",
-	password: "password",
-	password_confirmation: "password"
-)
+# User.create(
+# 	email: "ilya@aol.com",
+# 	password: "password",
+# 	password_confirmation: "password"
+# )
 
-User.create(
-	email: "cat@aol.com",
-	password: "password",
-	password_confirmation: "password"
-)
+# User.create(
+# 	email: "cat@aol.com",
+# 	password: "password",
+# 	password_confirmation: "password"
+# )
 
-tags = %w{no-wifi super-cold very-small open-late no-power-plugs}
-tags.each do |tag|
-	Tag.create(name: tag)
+# tags = %w{no-wifi super-cold very-small open-late no-power-plugs}
+# tags.each do |tag|
+# 	Tag.create(name: tag)
+# end
+
+names = %w{weekender galleries rainy-day}
+
+names.each do |name|
+	Collection.create(
+		name: name, 
+		user_id: 1
+	)
+end
+
+names.length.times do |i|
+	CollectionLocation.create(
+		collection_id: (i + 1),
+		location_id: 1
+	)
 end
