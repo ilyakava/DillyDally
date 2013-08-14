3eb7aad93a024b04fd51949a7fdc24304b805a99
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-14 12:53:26 -0400

    Finish writing location model method tests

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,13 +1,17 @@
 FactoryGirl.define do
-	factory :category do |f|
-		f.name "Food"
+	factory :category do
+		name "Food"
 	end
 
-	factory :location_category do |f|
+	factory :location_category do
 		category
 	end
 
-	factory :location do |f|
-		location_category
+	factory :location do
+		name "Test-time"
+	end
+
+	factory :location_with_category, parent: :location do
+		location_categories {[FactoryGirl.create(:location_category)]}
 	end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe Location do
-	it "can return its categories as an array"
-	it "can force categories to be assigned to it"
+	it "has a valid factory" do
+		FactoryGirl.create(:location_with_category).categories.
+			should_not be_empty
+	end
+
+	it "can return categories_as_array" do
+		location = FactoryGirl.create(:location_with_category)
+		location.categories_as_array.should == ["Food"]
+	end
+
+	describe "assign_categories_for a location" do
+		
+		let(:location) { FactoryGirl.create(:location) }
+
+		it "has no categories initially" do
+			location.categories.should be_empty
+		end
+
+		it "can have categories assigned to it" do
+			location.assign_categories_for(["Food"])
+			location.categories.should_not be_empty
+		end	
+	end
 end
