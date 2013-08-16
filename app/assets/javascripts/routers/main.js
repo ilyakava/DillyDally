6825f09317c6c73712430cbb9a61525a5b1448f2
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-16 16:32:28 -0400

    Realize subroutes is a better option

--- app/assets/javascripts/routers/main.js
+++ app/assets/javascripts/routers/main.js
@@ -1,9 +1,9 @@
+// in use
 DD.Routers.Main = Backbone.Router.extend({
   initialize: function ($headEl, $contentEl, userData) {
-    that.$headEl = $headEl;
-    that.$contentEl = $contentEl;
-    that.userData = userData;
-    // parse data first
+    this.$headEl = $headEl;
+    this.$contentEl = $contentEl;
+    this.userData = new DD.Models.User(userData);
   },
 
   routes: {
@@ -14,6 +14,15 @@ DD.Routers.Main = Backbone.Router.extend({
   },
 
   userCollections: function () {
+    var that = this;
+    var user = that.userData;
+    var userCollectionsView = DD.Views.UserCollections(
+      $headEl,
+      $contentEl,
+      user
+    );
+    // render is called as a command here
+
     // has a user, has her collections
     // render a view that accepts a user model, and renders collections
   },
---
+++ app/assets/javascripts/views/collections/user_collections_head.js
@@ -0,0 +1,19 @@
+// in use
+DD.Views.UserCollectionsHead = Backbone.View.extend({
+
+  events: {
+    "click button#recenter": "didYouMean"
+  },
+
+  didYouMean: function () {
+    console.log("triggered map recenter search method");
+    Backbone.history.navigate("#/redirecting");
+    Backbone.history.navigate("#/recenter-by-search");
+  },
+
+  render: function () {
+    var renderedHead = JST['collections/head']();
+    this.$el.html(renderedHead);
+    return this;
+  }
+});
