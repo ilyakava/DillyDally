47f95444cf10cb5a43e8b2455b6db17e3e76a8aa
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-18 12:27:22 -0400

    Change variable name

--- app/assets/javascripts/views/friends/list_users.js
+++ app/assets/javascripts/views/friends/list_users.js
@@ -4,8 +4,8 @@ DD.Views.UserFriends = Backbone.View.extend({
   // is a view that accepts user model, and renders all friends
 
   // collection: DD.Collections.Users,
-  initialize: function (user, $headEl) {
-    this.model = user;
+  initialize: function (userModel, $headEl) {
+    this.model = userModel;
     this.$headEl = $headEl;
 
     // render head of searchbar (tabs and recenter searchbar)
