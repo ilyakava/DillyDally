236ad8e4070c765b399c1938cc3087f2712e769f
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-20 22:08:16 -0400

    Make rails serve the tags to choose from

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,14 @@
 class TagsController < ApplicationController
 
+	def index
+		@tags = Tag.all
+
+		respond_to do |format|
+			format.json { render json: @tags.to_json}
+		end		
+	end
+
+
 	def create
 		@tag = Tag.new(params[:tag])
 		if @tag.save
