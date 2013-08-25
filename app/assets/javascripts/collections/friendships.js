e96d2f4d7e88f041701037c7387b2c7c5e41946e
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-24 22:54:53 -0400

    Enable graying out of already made friends

--- app/assets/javascripts/collections/friendships.js
+++ app/assets/javascripts/collections/friendships.js
@@ -4,11 +4,13 @@ DD.Collections.Friendships = Backbone.Collection.extend({
   parseAndSaveForm: function (serializedArray) {
     var that = this;
     _(serializedArray).each(function (object) {
-      var friend_id = object["value"];
+      var friend_id = object["value"].split("|")[0];
+      var email = object["value"].split("|")[1];
 
       that.create({
         friend_id: friend_id,
-        user_id: current_user.id
+        user_id: current_user.id,
+        friend: new DD.Models.User({email: email})
       });
     });
     return that;
