af8b6315d2d558084621fe3ddcba29a4fa274763
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-24 22:20:16 -0400

    Tailor child head views to the parents

--- app/assets/javascripts/views/collections/user_collections_head.js
+++ app/assets/javascripts/views/collections/user_collections_head.js
@@ -1,31 +1,8 @@
 // in use
-DD.Views.UserCollectionsHead = Backbone.View.extend({
-
-  events: {
-    "click button#recenter": "didYouMean",
-    "keyup input[type=text].recenter-box": "didYouMeanEnter"
-  },
-
-  didYouMeanEnter: function (event) {
-    var that = this;
-    if (event.keyCode === 13) {
-      that.didYouMean();
-    }
-  },
-
-  didYouMean: function () {
-    console.log("triggered map recenter search method");
-    Backbone.history.navigate("#/redirecting");
-    Backbone.history.navigate("#/recenter-by-search");
-  },
-
+DD.Views.UserCollectionsHead = DD.Views.Head.extend({
   render: function () {
     var renderedHead = JST['collections/head']();
     this.$el.html(renderedHead);
     return this;
-  },
-
-  cancel: function () {
-    // nothing yet
   }
 });
--- app/assets/javascripts/views/locations/locations_head.js
+++ app/assets/javascripts/views/locations/locations_head.js
@@ -1,16 +1,5 @@
 // in use
-DD.Views.LocationsHead = Backbone.View.extend({
-
-  events: {
-    "click button#recenter": "didYouMean"
-  },
-
-  didYouMean: function () {
-    console.log("triggered map recenter search method");
-    Backbone.history.navigate("#/redirecting");
-    Backbone.history.navigate("#/recenter-by-search");
-  },
-
+DD.Views.LocationsHead = DD.Views.Head.extend({
   render: function () {
     var renderedHead = JST['locations/head']();
     this.$el.html(renderedHead);
