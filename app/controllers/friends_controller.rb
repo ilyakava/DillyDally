328e884c8fc263a155d1dd1c94917f3c1d25c2fb
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-08-26 23:18:56 -0400

    Remove unneccessarily in depth query

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,10 +2,8 @@ class FriendsController < ApplicationController
 
 	def index
 		@friends = current_user.friends.includes(:locations, :collections)
-		@friends_json = @friends.to_json(
-			include: [{locations: {methods: [:categories_as_array],
-			include: [{comments: { include: :author }}, {location_tags: { include: :tag }}, :user_visits, :visitors, :savers, :creator]}}, {collections: {include: [{locations: { methods: :categories_as_array, include: :categories }}]}}]
-		)
+		@friends_json = @friends.to_json(include: [:locations, :collections])
+
 		@users_json = User.all.to_json
 
 		respond_to do |format|
